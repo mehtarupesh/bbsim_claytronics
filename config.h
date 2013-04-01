90b4f95cc435a95f9859a37d1d2b308f338a3b9e
Seth Goldstein <seth@flashgroup.com>
2013-04-01 09:34:35 -0400

    add code for doing a test

--- config.h
+++ config.h
@@ -1,7 +1,13 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
+char* configFile;
+int   configCount;
+int** testData;
+
+
 void randomConfig(int);
 void readConfig(char *name);
+int configtest(char* name);
 
 #endif
